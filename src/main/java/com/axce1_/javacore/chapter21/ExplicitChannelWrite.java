a4aa772543c87dae08c91a4ae2b196e2d9c23f0f
Evgeniy Popov <popovei@vezitaxi.com>
2020-06-05 16:14:24 +0500

    chapter21

---
+++ src/main/java/com/axce1_/javacore/chapter21/ExplicitChannelWrite.java
@@ -0,0 +1,31 @@
+package com.axce1_.javacore.chapter21;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class ExplicitChannelWrite {
+    public static void main(String[] args) {
+        try (
+                FileChannel fChan = (FileChannel) Files.newByteChannel(
+                        Paths.get("test.txt"),
+                        StandardOpenOption.WRITE,
+                        StandardOpenOption.CREATE)
+        ) {
+            ByteBuffer mBuf = ByteBuffer.allocate(26);
+            for(int i=0; i<26; i++)
+                mBuf.put((byte) ('A'+i));
+
+            mBuf.rewind();
+            fChan.write(mBuf);
+        } catch (InvalidPathException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
